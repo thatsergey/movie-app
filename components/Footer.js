4420052df2914de7cdceb1e8d9036916fcd1d340
thatsergey <sg2813716@gmail.com>
2023-05-29 20:31:37 +0300

    Finished creating index.js

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react"
+import styles from '../styles/Home.module.css'
+
+const Footer =() =>{
+   return (
+      <footer className={styles.footer}>
+         <div>
+            <span>Created by </span>
+            <a 
+            href="https://github.com/thatsergey?tab=repositories" 
+            target="_blank" 
+            rel="noopener noreferrer">
+            </a>
+         </div>
+         <span>Sergey</span>
+      </footer>
+   )
+}
+export default Footer
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Footer from '../components/footer'
 import '../styles/globals.css'
+import '../styles/main.css'
+import styles from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <div className={styles.container}>
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  ) 
 }
 
 export default MyApp
